46dfb976541c72634744c86c8b9d1bc9d2d1ce31
selfresonator <selfresonator@gmail.com>
2015-12-04 16:16:43 -0800

    eloquentjs updates

--- eloquentjavascript/minimum.js
+++ eloquentjavascript/minimum.js
@@ -1,9 +1,15 @@
-function min(a, b) {
-	if (a < b) {
-		console.log(a);
-	} else {
-		console.log(b);
-	}
-};
+//  long min func i came up with:
+//  function min(a, b) {
+//  if (a < b) {
+//  		console.log(a);
+//  	} else {
+//  		console.log(b);
+//  	}
+//  };
+
+//refactored min func
+var min = function(a,b) {(a < b) ? console.log(a) : console.log(b)};
 
 min(4300,56789);
+min(234667,10);
+min(284, 1000983)
