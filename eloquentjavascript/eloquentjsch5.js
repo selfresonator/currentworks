04af95d19cdb921b685b1d1d3f6de73f91e40bc7
selfresonator <selfresonator@gmail.com>
2015-12-05 01:48:03 -0800

    added chapter 5 reduce func exercise

--- eloquentjavascript/eloquentjsch5.js
+++ eloquentjavascript/eloquentjsch5.js
@@ -8,6 +8,7 @@ console.log(every([NaN, NaN, NaN], isNaN));
 console.log(every([NaN, NaN, 4], isNaN));
 
 var some = function(array,predicate) {
+
 	for(var i = 0; i < array.length; i ++) {
 	  predicate(array[i]) ? console.log(true) : console.log(false)
 	}
@@ -15,3 +16,19 @@ var some = function(array,predicate) {
 
 console.log(some([NaN, 3, 4], isNaN));
 console.log(some([2, 3, 4], isNaN));
+
+
+// FLATTENING:
+var reduce = function(array,combine) {
+	return combine(array);
+};
+
+nums = [[3,45,724],[695,45],[33,2,514],[684,55,90],[64,3]];
+
+console.log(reduce(nums,function(array) {
+	var newArr = [];
+	for(var i = 0; i < array.length; i++) {
+		newArr.push(array[i]);
+	}
+	return newArr;
+}));
