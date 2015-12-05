b973288eb79c8a62810cbd085c77c3919c4e0233
selfresonator <selfresonator@gmail.com>
2015-12-04 16:37:45 -0800

    worked on ch5 exercises

--- eloquentjavascript/eloquentjsch5.js
+++ eloquentjavascript/eloquentjsch5.js
@@ -1,13 +1,17 @@
-var testArray = [1,2,3,4,5,6,22,13,23,64,86];
-var newArray = []
-forEach(array, function( {
-	for(i = 0; i < array.length; i++) {
-	newArray.push(array[i * 34])
-	
+//  CH 5 EXERCISES
+//  EVERY AND SOME:
+var every = function(array,predicate) {
+	predicate ? console.log(true) : console.log(false)
+}
+
+console.log(every([NaN, NaN, NaN], isNaN));
+console.log(every([NaN, NaN, 4], isNaN));
+
+var some = function(array,predicate) {
+	for(var i = 0; i < array.length; i ++) {
+	  predicate(array[i]) ? console.log(true) : console.log(false)
 	}
-	)
-);
+}
 
-
-console.log(testArray);
-console.log(newArray);
+console.log(some([NaN, 3, 4], isNaN));
+console.log(some([2, 3, 4], isNaN));
