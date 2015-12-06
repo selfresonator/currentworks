ed7a74fccba091f808c2035ea4cff04e76997865
selfresonator <selfresonator@gmail.com>
2015-12-06 13:27:33 -0800

    updated reverse array function

--- elojs/reversinganarray.js
+++ elojs/reversinganarray.js
@@ -1,5 +1,10 @@
-//function reverseArray(array) {
-//	for (i = 0; i < array.length; i++) {
-//		array[i] = array[]
-//	}
-//};
+function reverseArray(array) {
+  var newArr = [];
+	for (i = array.length-1; i >= 0; i--) {
+    newArr.push(array[i]);
+	}
+  return newArr;
+};
+
+var testArr = [24,631,4,49025];
+console.log(reverseArray(testArr));
